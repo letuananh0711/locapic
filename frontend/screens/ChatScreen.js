e91e26f085c54f782c11cee89b4114c6d88a6608
tuan-anh.le <tuan-anh.le@cerema.fr>
2022-07-21 21:53:13 +0200

    add style for chat screen part 3

--- frontend/screens/ChatScreen.js
+++ frontend/screens/ChatScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, ScrollView, SafeAreaView, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, ScrollView, SafeAreaView, KeyboardAvoidingView, Text, View } from 'react-native';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import { Input, Button, ListItem } from 'react-native-elements'
 import { connect } from 'react-redux';
@@ -6,7 +6,7 @@ import { connect } from 'react-redux';
 import socketIOClient from "socket.io-client";
 import { useEffect, useState } from 'react';
 
-const socket = socketIOClient("http://192.168.1.20:3000");
+const socket = socketIOClient("http://192.168.0.169:3000");
 
 function ChatScreen(props) {
     const [listMessage, setListMessage] = useState([]);
@@ -23,24 +23,25 @@ function ChatScreen(props) {
 
     useEffect(() => {
         socket.on('sendMessageToAll', (newMessage) => {
-            let emojiMessage = newMessage.currentMessage.replace(emojiRegex, (m)=> emojiMap[m.toLowerCase()]);
+            let emojiMessage = newMessage.currentMessage.replace(emojiRegex, (m) => emojiMap[m.toLowerCase()]);
             let fuckWordMessage = emojiMessage.replace(fuckWRegex, '\u2022\u2022\u2022');
-            setListMessage([...listMessage, {...newMessage, currentMessage: fuckWordMessage}]);
+            setListMessage([...listMessage, { ...newMessage, currentMessage: fuckWordMessage }]);
         });
+        return () => socket.off();
     }, [listMessage]);
+
 
     return (
         <SafeAreaView style={styles.container}>
             <ScrollView>
                 {
-                    listMessage.map((msg, i) => (
-                        <ListItem key={i}>
-                            <ListItem.Content>
-                                <ListItem.Title>{msg.currentMessage}</ListItem.Title>
-                                <ListItem.Subtitle>{msg.pseudo}</ListItem.Subtitle>
-                            </ListItem.Content>
-                        </ListItem>
-                    ))
+                    listMessage.map((message, i) => 
+                        (
+                            <View key={i} style={message.pseudo===props.pseudo ? styles.messageRight : styles.message}>
+                                <Text style={message.pseudo===props.pseudo ? styles.titleRight : styles.title}>{message.currentMessage}</Text>
+                                <Text style={message.pseudo===props.pseudo ? styles.subtitleRight : styles.subtitle}>{message.pseudo}</Text>
+                            </View>
+                        ))
                 }
             </ScrollView>
 
@@ -53,7 +54,7 @@ function ChatScreen(props) {
                 <Button
                     title='Send'
                     onPress={() => {
-                        socket.emit('sendMessage', {currentMessage: currentMessage, pseudo: props.pseudo});
+                        socket.emit('sendMessage', { currentMessage: currentMessage, pseudo: props.pseudo });
                         setCurrentMessage('')
                     }}
                     icon={
@@ -77,6 +78,40 @@ const styles = StyleSheet.create({
     },
     btnContainerSend: {
         backgroundColor: '#eb4d4b',
+    },
+    message: {
+        backgroundColor: '#8a8d91',
+        padding: 20,
+        marginVertical: 5,
+        marginLeft: 16,
+        marginRight: '50%',
+        flex: 1,
+        borderRadius: 20,
+        maxWidth: '50%',
+    },
+    title: {
+        fontSize: 16,
+    },
+    subtitle: {
+        fontSize: 12,
+    },
+    messageRight: {
+        backgroundColor: 'rgb(0, 132, 255)',
+        padding: 20,
+        marginVertical: 5,
+        marginRight: 16,
+        marginLeft: '50%',
+        flex: 1,
+        borderRadius: 20,
+        maxWidth: '50%',
+    },
+    titleRight: {
+        fontSize: 16,
+        textAlign: 'right',
+    },
+    subtitleRight: {
+        fontSize: 12,
+        textAlign: 'right',
     }
 });
 
